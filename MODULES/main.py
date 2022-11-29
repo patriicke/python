169f3a8121b55eb0e705fbbc17cc9981705cfad6
patriicke <patrickndayambaje4@gmail.com>
2022-11-29 22:21:29 +0200

    [update]: I have added modules

---
+++ MODULES/main.py
@@ -0,0 +1,5 @@
+# I have imported all functions and variables in mymodule and renamed my module to mx
+import mymodule as mx
+from other1 import numbers  # I have imported only number from other1 module
+print(mx.fruits)
+print(numbers)
---
+++ MODULES/mymodule.py
@@ -0,0 +1,5 @@
+def greet(name):
+    print("Hi! My name is " + name)
+
+
+fruits = ["mango", "tomato", "strawberry"]
