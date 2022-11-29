095f3272074462c945166cf02975c1029d569a5d
patriicke <patrickndayambaje4@gmail.com>
2022-11-29 06:55:35 +0200

    [update]: These are python variables

---
+++ VARIABLES/variables.py
@@ -0,0 +1,23 @@
+# many variable to many values
+x,y,z = "Patrick", "Pacifique", "Price"
+print(x)
+print(y)
+print(z)
+
+# one value to multiple variables
+
+a = b = c = "Jane"
+
+print(a)
+print(b)
+print(c)
+
+#unpacking a collection example
+
+fruits = ["Mango", "Banana", "Orange"]
+i,j,k = fruits
+print(i)
+print(j)
+print(k)
+
+
