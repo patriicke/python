00fd99369253a25ddaae85f7afb7e1a6a4dcdbdd
patriicke <patrickndayambaje4@gmail.com>
2022-11-29 21:51:23 +0200

    [update]: This python classes

---
+++ CLASS/class.py
@@ -0,0 +1,11 @@
+class Person:
+    def __init__(self, firstname, lastname, age):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.age = age
+
+
+p1 = Person("Patrick", "NDAYAMBAJE", 20)
+print(p1.firstname)
+print(p1.lastname)
+print(p1.age)
